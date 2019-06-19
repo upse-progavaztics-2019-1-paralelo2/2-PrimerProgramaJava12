cb5009014d1f26449b3f3e1b9dc7e4cfb4050bf3
ivansanchezvera <ivansanchez_1988@hotmail.com>
2019-06-18 21:09:07 -0500

    Primer programa con Java12

---
+++ src/FirstProgram.java
@@ -0,0 +1,35 @@
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+
+		double iva=12, precio;
+		
+		
+		
+		
+		
+		iva = 12;
+		double numero = 21.23;
+		System.out.println(numero);
+		
+		String s = "";
+		System.out.println(s);
+		
+		String nombrePersona = args[0];
+		String nombre2 = args[1];
+		nombrePersona = "test";
+		System.out.println("Todo chevere!");
+		int x = 0;
+
+		do {
+			x++;
+			System.out.println(x);
+		} while (x < 20);
+
+		System.out.println("Hola " + nombrePersona);
+		System.out.println("Que ves con " + nombre2);
+		System.out.println("El partido de la champions!");
+	}
+
+}
